9e5e50a4bf5e89bb33d5f4499116e8cf75b11d75
Ahmad <134114334+AhmadXDev@users.noreply.github.com>
2024-12-03 18:37:49 +0300

    homework1

---
+++ bin/dart_course_day3.dart
@@ -0,0 +1,27 @@
+// Acc
+
+// Lower count = 2
+// total = 3
+
+// Total/2 = 1 > Lower Count
+
+// String checkCase(String str) {
+//   int count = 0;
+//   for(int i=0; i<)
+// }
+
+
+
+void countLetters(String str){ 
+  Map<String, int> map = {}; 
+  str.replaceAll(" ", ""); 
+  for(int i=0; i<str.length; i++){ 
+    if(map.containsKey(str[i])){
+      map[str[i]] = map[str[i]]! + 1;
+        } else {
+      map[str[i]] = 1;
+        }
+      }
+      print(map);
+    }
+  
---
+++ test/dart_course_day3_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_course_day3/dart_course_day3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
